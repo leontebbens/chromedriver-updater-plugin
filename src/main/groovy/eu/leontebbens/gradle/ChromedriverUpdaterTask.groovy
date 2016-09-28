606b3d162901df42c219784ef04f44cbec50b73e
Elad Cohen <elad.cohen@hpe.com>
2016-09-28 21:31:31 +0300

    Fix update chrome driver that failed due to mac 32 bit that doesn't exist anymore

--- src/main/groovy/eu/leontebbens/gradle/ChromedriverUpdaterTask.groovy
+++ src/main/groovy/eu/leontebbens/gradle/ChromedriverUpdaterTask.groovy
@@ -11,6 +11,7 @@ class ChromedriverUpdaterTask extends DefaultTask {
     def chromedriverSiteUrl = "http://chromedriver.storage.googleapis.com"
     def targetDir = "${project.buildDir}/chromedriver"
     def LOCAL_VER = "$targetDir/LOCAL_VERSION"
+    def driverLocation = getDriverLocation();
 
     ChromedriverUpdaterTask() {
         setGroup("Build")
@@ -26,12 +27,23 @@ class ChromedriverUpdaterTask extends DefaultTask {
             isChromedriverUptodate()
         } else {
             updateChromedriver()
-            def osName = System.getProperty("os.name").toLowerCase()
-            def arch = osName.contains("windows") ? "win" : osName.contains("mac") ? "mac" : "linux"
-            def driver = "$targetDir/$arch/chromedriver${arch.equals('win') ? '.exe' : ''}"
-            println("webdriver.chrome.driver=$driver")
-            System.setProperty("webdriver.chrome.driver", "$driver")
+            println("webdriver.chrome.driver=$driverLocation")
         }
+    }
+
+    /**
+     * let other gradle tasks to use this driver location
+     * @return downloaded chrome driver location on the disk
+     */
+    public String getDriverLocation() {
+        def arch = calcArch()
+        driverLocation = "$targetDir/$arch/chromedriver${arch.equals('win') ? '.exe' : ''}"
+        return driverLocation
+    }
+
+    private String calcArch() {
+        def osName = System.getProperty("os.name").toLowerCase()
+        return osName.contains("windows") ? "win" : osName.contains("mac") ? "mac" : "linux"
     }
 
     private isChromedriverUptodate() {
@@ -51,9 +63,9 @@ class ChromedriverUpdaterTask extends DefaultTask {
             new File(project.buildDir.toString()).mkdir()
             println("Downloading Chromedriver $latestVersion from $latestDriverBaseUrl ...")
             downloadFile("$chromedriverSiteUrl/LATEST_RELEASE", LOCAL_VER)
-            downloadAndUnzip("$latestDriverBaseUrl", "chromedriver_mac32.zip", "$targetDir/mac")
-            downloadAndUnzip("$latestDriverBaseUrl", "chromedriver_win32.zip", "$targetDir/win")
-            downloadAndUnzip("$latestDriverBaseUrl", "chromedriver_linux64.zip", "$targetDir/linux")
+            def arch = calcArch()
+            def bit = arch.equals('win') ? '32' : '64'
+            downloadAndUnzip("$latestDriverBaseUrl", "chromedriver_${arch}${bit}.zip", "$targetDir/${arch}")
             println("Download complete: the latest Chromedriver is available in $targetDir")
         }
     }
@@ -94,8 +106,10 @@ class ChromedriverUpdaterTask extends DefaultTask {
     }
 
     private void downloadAndUnzip(def remoteZipURL, def zipName, String localDir) {
-        downloadFile("$remoteZipURL/$zipName", "$targetDir/tempZip")
+        String tmpZipFile = "$targetDir/tempZip"
+        downloadFile("$remoteZipURL/$zipName", tmpZipFile)
         new File(localDir).mkdirs()
-        new AntBuilder().unzip(src: "$targetDir/tempZip", dest: localDir)
+        new AntBuilder().unzip(src: tmpZipFile, dest: localDir)
+        new File(tmpZipFile).delete();
     }
 }
